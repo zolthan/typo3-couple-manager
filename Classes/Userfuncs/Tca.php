ec41995195962e2adbc449a8a1a3a6551fd0b544
Zolthan Rodiak <wilhelm79@web.de>
2018-08-11 19:20:48 +0200

    - added select items

--- Classes/Userfuncs/Tca.php
+++ Classes/Userfuncs/Tca.php
@@ -17,6 +17,8 @@ use SchwarzWeissReutlingen\CoupleManager\Domain\Model\Couple;
 use SchwarzWeissReutlingen\CoupleManager\Domain\Repository\CoupleRepository;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Object\ObjectManager;
+use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
+use TYPO3\CMS\Lang\LanguageService;
 
 /**
  * Tca renderer
@@ -27,7 +29,12 @@ class Tca
     protected $objectManager;
     /** @var CoupleRepository */
     protected $coupleRepository;
+    /** @var LocalizationUtility */
+    protected $localize;
 
+    /**
+     * Tca constructor.
+     */
     public function __construct()
     {
         $this->objectManager = GeneralUtility::makeInstance(ObjectManager::class);
@@ -35,10 +42,70 @@ class Tca
     }
 
 
+    /**
+     * @param $parameters
+     * @param $parentObject
+     */
     public function getCoupleName(&$parameters, $parentObject)
     {
         /** @var Couple $couple */
         $couple = $this->coupleRepository->findByUid($parameters['row']['uid']);
         $parameters['title'] = $couple->getCoupleName();
+    }
+
+    /**
+     * @return array
+     */
+    public function getDisciplineItems($config)
+    {
+        $config['items'] = [
+            [LocalizationUtility::translate('please_choose', 'couple_manager'), 0],
+            [LocalizationUtility::translate('standard', 'couple_manager'), 'standard'],
+            [LocalizationUtility::translate('latin', 'couple_manager'), 'latin'],
+            [LocalizationUtility::translate('10dance', 'couple_manager'), '10dance'],
+        ];
+        return $config;
+    }
+
+    /**
+     * @return array
+     */
+    public function getStartingClassItems($config)
+    {
+        $config['items'] = [
+            [LocalizationUtility::translate('please_choose', 'couple_manager'), 0],
+            ['S', 's'],
+            ['A', 'a'],
+            ['B', 'b'],
+            ['C', 'c'],
+            ['D', 'd'],
+        ];
+        return $config;
+    }
+
+    /**
+     * @return array
+     */
+    public function getStartingGroupItems($config)
+    {
+
+
+        $config['items'] = [
+            [LocalizationUtility::translate('please_choose', 'couple_manager'), 0],
+            [LocalizationUtility::translate('kinder', 'couple_manager'), 'kin'],
+            [LocalizationUtility::translate('kinder1', 'couple_manager'), 'kin1'],
+            [LocalizationUtility::translate('kinder2', 'couple_manager'), 'kin2'],
+            [LocalizationUtility::translate('junioren1', 'couple_manager'), 'jun1'],
+            [LocalizationUtility::translate('junioren2', 'couple_manager'), 'jun2'],
+            [LocalizationUtility::translate('jugend', 'couple_manager'), 'jug'],
+            [LocalizationUtility::translate('hauptgruppe', 'couple_manager'), 'hgr'],
+            [LocalizationUtility::translate('hauptgruppe2', 'couple_manager'), 'hgr2'],
+            [LocalizationUtility::translate('senioren1', 'couple_manager'), 'sen1'],
+            [LocalizationUtility::translate('senioren2', 'couple_manager'), 'sen2'],
+            [LocalizationUtility::translate('senioren3', 'couple_manager'), 'sen3'],
+            [LocalizationUtility::translate('senioren4', 'couple_manager'), 'sen4'],
+            [LocalizationUtility::translate('senioren5', 'couple_manager'), 'sen5'],
+        ];
+        return $config;
     }
 }
