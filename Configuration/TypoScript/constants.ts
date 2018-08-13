e694d705f979d1c6ac001cad9903367eed7702d7
Zolthan Rodiak <wilhelm79@web.de>
2018-08-13 19:27:48 +0200

    - some less - and some renaming

--- Configuration/TypoScript/constants.ts
+++ Configuration/TypoScript/constants.ts
@@ -1,15 +1,15 @@
-
-plugin.tx_couplemanager_couple {
+plugin.tx_couplemanager {
     view {
-        # cat=plugin.tx_couplemanager_couple/file; type=string; label=Path to template root (FE)
+        # cat=plugin.tx_couplemanager/file; type=string; label=Path to template root (FE)
         templateRootPath = EXT:couple_manager/Resources/Private/Templates/
-        # cat=plugin.tx_couplemanager_couple/file; type=string; label=Path to template partials (FE)
+        # cat=plugin.tx_couplemanager/file; type=string; label=Path to template partials (FE)
         partialRootPath = EXT:couple_manager/Resources/Private/Partials/
-        # cat=plugin.tx_couplemanager_couple/file; type=string; label=Path to template layouts (FE)
+        # cat=plugin.tx_couplemanager/file; type=string; label=Path to template layouts (FE)
         layoutRootPath = EXT:couple_manager/Resources/Private/Layouts/
     }
+
     persistence {
-        # cat=plugin.tx_couplemanager_couple//a; type=string; label=Default storage PID
+        # cat=plugin.tx_couplemanager//a; type=string; label=Default storage PID
         storagePid = 137
     }
 }
--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -1,17 +1,18 @@
-
-plugin.tx_couplemanager_couple {
+plugin.tx_couplemanager {
     view {
         templateRootPaths.0 = EXT:couple_manager/Resources/Private/Templates/
-        templateRootPaths.1 = {$plugin.tx_couplemanager_couple.view.templateRootPath}
+        templateRootPaths.1 = {$plugin.tx_couplemanager.view.templateRootPath}
         partialRootPaths.0 = EXT:couple_manager/Resources/Private/Partials/
-        partialRootPaths.1 = {$plugin.tx_couplemanager_couple.view.partialRootPath}
+        partialRootPaths.1 = {$plugin.tx_couplemanager.view.partialRootPath}
         layoutRootPaths.0 = EXT:couple_manager/Resources/Private/Layouts/
-        layoutRootPaths.1 = {$plugin.tx_couplemanager_couple.view.layoutRootPath}
+        layoutRootPaths.1 = {$plugin.tx_couplemanager.view.layoutRootPath}
     }
+
     persistence {
-        storagePid = {$plugin.tx_couplemanager_couple.persistence.storagePid}
+        storagePid = {$plugin.tx_couplemanager.persistence.storagePid}
         #recursive = 1
     }
+
     features {
         #skipDefaultArguments = 1
         # if set to 1, the enable fields are ignored in BE context
@@ -19,41 +20,9 @@ plugin.tx_couplemanager_couple {
         # Should be on by default, but can be disabled if all action in the plugin are uncached
         requireCHashArgumentForActionArguments = 1
     }
+
     mvc {
-        #callDefaultActionIfActionCantBeResolved = 1
+        callDefaultActionIfActionCantBeResolved = 1
     }
 }
 
-# these classes are only used in auto-generated templates
-plugin.tx_couplemanager._CSS_DEFAULT_STYLE (
-    textarea.f3-form-error {
-        background-color:#FF9F9F;
-        border: 1px #FF0000 solid;
-    }
-
-    input.f3-form-error {
-        background-color:#FF9F9F;
-        border: 1px #FF0000 solid;
-    }
-
-    .tx-couple-manager table {
-        border-collapse:separate;
-        border-spacing:10px;
-    }
-
-    .tx-couple-manager table th {
-        font-weight:bold;
-    }
-
-    .tx-couple-manager table td {
-        vertical-align:top;
-    }
-
-    .typo3-messages .message-error {
-        color:red;
-    }
-
-    .typo3-messages .message-ok {
-        color:green;
-    }
-)
