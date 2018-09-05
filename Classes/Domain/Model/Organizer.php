e51c729448d5b7ed40c6510d15dbd70d9f8818fa
Zolthan Rodiak <wilhelm79@web.de>
2018-09-05 20:20:27 +0200

    [TASK] new organizer structure

---
+++ Classes/Domain/Model/Organizer.php
@@ -0,0 +1,75 @@
+<?php
+namespace SchwarzWeissReutlingen\CoupleManager\Domain\Model;
+
+/***
+ *
+ * This file is part of the "Couple Manager" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ *  (c) 2018 Sebastian Wilhelm <wilhelm79@web.de>, Schwarz-Weiß Reutlingen e.V.
+ *
+ ***/
+
+/**
+ * CompetitionType
+ */
+class Organizer extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity
+{
+    /**
+     * name
+     *
+     * @var string
+     */
+    protected $name = '';
+
+    /**
+     * city
+     *
+     * @var string
+     */
+    protected $city = '';
+
+    /**
+     * Returns the name
+     *
+     * @return string $name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Sets the name
+     *
+     * @param string $name
+     * @return void
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Returns the city
+     *
+     * @return string $city
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * Sets the city
+     *
+     * @param string $city
+     * @return void
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+    }
+}
---
+++ Classes/Domain/Repository/OrganizerRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace SchwarzWeissReutlingen\CoupleManager\Domain\Repository;
+
+/***
+ *
+ * This file is part of the "Couple Manager" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ *  (c) 2018 Sebastian Wilhelm <wilhelm79@web.de>, Schwarz-Weiß Reutlingen e.V.
+ *
+ ***/
+
+/**
+ * The repository for Organizers
+ */
+class OrganizerRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
+{
+}
